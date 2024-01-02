eb4a2951db785c876a7917c882327a2bba85ea51
Horacio Herrera <hi@horacioh.com>
2024-01-02 14:52:16 +0100

    node 20 + remove reporter config

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ const config: PlaywrightTestConfig = {
     timeout: 10 * 1000,
   },
   timeout: 10 * 1000,
-  reporter: process.env.CI ? 'github' : 'list',
+  // reporter: process.env.CI ? 'github' : 'list',
   // reporter: [
   //   ['list'],
   //   [
